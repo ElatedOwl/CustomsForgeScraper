97434f4f97a4d76cca636fe8064b760dcaa540e5
Tyler Gall <tylerjgall+github@gmail.com>
2019-06-04 14:54:04 -0500

    Changed all URLs to https, added BASE_URL

--- CFScraper/Contracts/Constants/CustomsForgeUrls.cs
+++ CFScraper/Contracts/Constants/CustomsForgeUrls.cs
@@ -7,7 +7,8 @@ namespace CFScraper.Contracts.Constants
     internal class CustomsForgeUrls
     {
         public const string LOGIN_PAGE = "https://customsforge.com/index.php?app=core&module=global&section=login";
-        public const string LOGIN_ACTION_PAGE = "http://customsforge.com/index.php?app=core&module=global&section=login&do=process";
-        public const string SEARCH_HANDLER = "http://ignition.customsforge.com/cfss?u=21575";
+        public const string LOGIN_ACTION_PAGE = "https://customsforge.com/index.php?app=core&module=global&section=login&do=process";
+        public const string SEARCH_HANDLER = "https://ignition.customsforge.com/cfss?u=21575";
+        public const string BASE_URL = "https://customsforge.com";
     }
 }
